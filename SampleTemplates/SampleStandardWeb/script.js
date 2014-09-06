c8e671124e4c29122581c48db9e841caaa3be5f9
Gregg Milligan <gmilligan@human-element.com>
2014-09-06 19:13:15 -0400

    added default sample templates

---
+++ SampleTemplates/SampleStandardWeb/script.js
@@ -0,0 +1,10 @@
+<<filename:l:[folder]/js:.>>
+<<var:n:project folder => [folder]>>
+
+jQuery(document).ready(function(){
+	/*your document-ready code here*/
+	//window-ready happens AFTER document-ready
+	jQuery(window).ready(function(){
+		/*your window-ready code here*/
+	});
+});
